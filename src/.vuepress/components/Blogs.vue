9a70f8d1309c96669455ef4fcb5dc65a96c39eb5
ryoum-0930 <dara.ryouma@gmail.com>
2019-11-09 23:32:28 +0900

    first commit

---
+++ src/.vuepress/components/Blogs.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-for="post in posts">
+    <a href="post.path">{{post.title}}</a>
+  </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            posts() {
+                return this.$site.pages
+                    .filter(page => page.type === 'post');
+            }
+        }
+    }
+</script>
